5a004a279047b7cb8d415df4a5bef6277858d166
Vipsu <meiram@bk.ru>
2023-09-20 21:54:09 +0600

    Example010

---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {15,32,83,34,18,25,63,75,18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
